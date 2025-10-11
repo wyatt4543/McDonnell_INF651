0828ec07ada8d40fbf414d6d11fd9493098523a9
wyatt4543 <w4543w@gmail.com>
2025-10-11 02:03:07 -0500

    save progress

---
+++ MovieWatchList/js/main.js
@@ -0,0 +1,45 @@
+let watchlist = {
+    movies: [],
+
+    //Method to add a movie to the watchlist
+    addMovie(movie) {
+        this.movies.push(movie);
+    },
+
+    displayMovies() {
+        let moviesList = document.getElementById("movie-list");
+        moviesList.innerHTML = "";
+
+        this.movies.forEach((movie, index) => {
+            let movieElement = document.createElement("li");
+            movieElement.className = "collection-item";
+
+            movieElement.innerHTML = `Movie: ${movie.name} Genre: ${movie.genre}<button onclick=watchlist.removeMovie(${index}) id="remove-movie-btn" class="remove-btn" type="button">
+            Mark As Watched</button>`;
+            moviesList.appendChild(movieElement);
+        });
+    },
+
+    removeMovie(index) {
+        this.movies.splice(index, 1);
+        this.displayMovies();
+    },
+};
+
+document.getElementById("add-movie-btn").addEventListener("click", function () {
+    let movieName = document.getElementById("movie-name").value;
+
+    if (movieName) {
+        let newMovie = {
+            name: movieName,
+        };
+
+        watchlist.addMovie(newMovie);
+
+        watchlist.displayMovies();
+
+        document.getElementById("movie-name").value = "";
+    } else {
+        alert("Please enter valid information");
+    }
+});
